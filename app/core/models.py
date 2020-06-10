a29e78a6933636596b9dcd1fb8e499af30d6d9a2
Ren Wako <maggots00002@gmail.com>
2020-06-10 21:13:12 +0900

    Added list recipe feature

--- app/core/models.py
+++ app/core/models.py
@@ -71,6 +71,7 @@ class Recipe(models.Model):
         on_delete=models.CASCADE
     )
 
+    name = models.CharField(max_length=255, blank=True)
     title = models.CharField(max_length=255)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
